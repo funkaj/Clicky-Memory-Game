7e5bab20a618ad48f15c46b1c75afe5cb0d16f3d
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-02 11:13:25 -0700

    Got images of characters and updated cards and formatting to get the right layout.

--- src/App.js
+++ src/App.js
@@ -2,31 +2,54 @@ import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import FriendCard from "./components/FriendCard";
+import Instructions from "./components/Instructions";
 import friends from "./friends.json";
 
 function App() {
-  return (
-    <Wrapper>
-      <Title>Friends List</Title>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
-      />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
-      />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
-      />
+  return ( <div class="d-flex justify-content-center">
+  <Wrapper>
+    <Title >Clicky Memory Game!</Title> 
+    <br />
+    <Instructions>Instructions go here in text!</Instructions>
+    <FriendCard image = {
+      friends[0].image
+    }/> 
+    <FriendCard image = {
+      friends[1].image
+    }/> 
+    <FriendCard image = {
+      friends[2].image
+    }/> 
+    <FriendCard image = {
+      friends[3].image
+    }/> 
+    <FriendCard image = {
+      friends[4].image
+    }/> 
+    <FriendCard image = {
+      friends[5].image
+    }/> 
+    <FriendCard image = {
+      friends[6].image
+    }/> 
+    <FriendCard image = {
+      friends[7].image
+    }
+    /> 
+    <FriendCard image = {
+      friends[8].image
+    }/> 
+    <FriendCard image = {
+      friends[9].image
+    }/> 
+    <FriendCard image = {
+      friends[10].image
+    }/> 
+    <FriendCard image = {
+      friends[11].image
+    }/> 
     </Wrapper>
+    </div>
   );
 }
 
