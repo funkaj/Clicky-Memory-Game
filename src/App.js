e3a4f0435aecfaef9f978ade94ae00c8cbef4304
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-02 11:58:12 -0700

    Added in an alt attribut to the images and also a data-id attribute. Need to figure out how to keep track of each card after shuffling the array and add in a state for 'clicked'.

--- src/App.js
+++ src/App.js
@@ -11,43 +11,68 @@ function App() {
     <Title >Clicky Memory Game!</Title> 
     <br />
     <Instructions>Instructions go here in text!</Instructions>
-    <FriendCard image = {
-      friends[0].image
-    }/> 
-    <FriendCard image = {
-      friends[1].image
-    }/> 
-    <FriendCard image = {
-      friends[2].image
-    }/> 
-    <FriendCard image = {
-      friends[3].image
-    }/> 
-    <FriendCard image = {
-      friends[4].image
-    }/> 
-    <FriendCard image = {
-      friends[5].image
-    }/> 
-    <FriendCard image = {
-      friends[6].image
-    }/> 
-    <FriendCard image = {
-      friends[7].image
-    }
-    /> 
-    <FriendCard image = {
-      friends[8].image
-    }/> 
-    <FriendCard image = {
-      friends[9].image
-    }/> 
-    <FriendCard image = {
-      friends[10].image
-    }/> 
-    <FriendCard image = {
-      friends[11].image
-    }/> 
+    <FriendCard
+      id={friends[0].id}
+      name={friends[0].name}
+      image={friends[0].image}
+    />
+    <FriendCard
+      id={friends[1].id}
+      name={friends[1].name}
+      image={friends[1].image}
+    />
+    <FriendCard
+      id={friends[2].id}
+      name={friends[2].name}
+      image={friends[2].image}
+    />
+    <FriendCard
+      id={friends[3].id}
+      name={friends[3].name}
+      image={friends[3].image}
+    />
+    <FriendCard
+      id={friends[4].id}
+      name={friends[4].name}
+      image={friends[4].image}
+    />
+    <FriendCard
+      id={friends[5].id}
+      name={friends[5].name}
+      image={friends[5].image}
+    />
+    <FriendCard
+      id={friends[6].id}
+      name={friends[6].name}
+      image={friends[6].image}
+    />
+    <FriendCard
+      id={friends[7].id}
+      name={friends[7].name}
+      image={friends[7].image}
+    />
+    <FriendCard
+      id={friends[8].id}
+      name={friends[8].name}
+      image={friends[8].image}
+    />
+    <FriendCard
+      id={friends[9].id}
+      name={friends[9].name}
+      image={friends[9].image}
+    />
+    <FriendCard
+      id={friends[10].id}
+      name={friends[10].name}
+      image={friends[10].image}
+    />
+    <FriendCard
+      id={friends[11].id}
+      name={friends[11].name}
+      image={friends[11].image}
+    />
+    
+    
     </Wrapper>
     </div>
   );
--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function FriendCard(props) {
   return (
     <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
+      <div className="img-container" data-id={props.id}>
+        <img alt={`${props.name} from bobs burgers`} src={props.image} />
       </div>
     </div>
   );
