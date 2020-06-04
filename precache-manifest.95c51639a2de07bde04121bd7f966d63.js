e4d99755df52c16e69d7aedc4422738c03edee02
Aaron Garner <aaron.garner.1982@gmail.com>
2020-06-04 12:17:27 -0700

    Updates

---
+++ precache-manifest.95c51639a2de07bde04121bd7f966d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45e811512667f50913e39f3c601b0b2",
+    "url": "/Clicky-Memory-Game/index.html"
+  },
+  {
+    "revision": "778d2e3c58b2aaedca23",
+    "url": "/Clicky-Memory-Game/static/css/main.edfe0b6c.chunk.css"
+  },
+  {
+    "revision": "8d9cf1cf6c3fc8dc5619",
+    "url": "/Clicky-Memory-Game/static/js/2.d7921de6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Memory-Game/static/js/2.d7921de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778d2e3c58b2aaedca23",
+    "url": "/Clicky-Memory-Game/static/js/main.f5eae2a1.chunk.js"
+  },
+  {
+    "revision": "3c73db0e8a750f0b4c00",
+    "url": "/Clicky-Memory-Game/static/js/runtime-main.ea6b19a8.js"
+  }
+]);
--- src/components/Instructions/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Instructions(props) {
-  return <div className="instructions justify-content-center">{props.children}</div>;
-}
-
-export default Instructions;
