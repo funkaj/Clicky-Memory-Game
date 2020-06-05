bce5604109d24bd9010ad2e64060a3e6ff910dc4
Adam <b.kimate@gmail.com>
2020-06-04 18:45:18 -0700

    update

---
+++ precache-manifest.1c841e15d9af7fefd2e17217ef8c76ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d287c6a7729fcb6463ebb8b45d2a88c",
+    "url": "/Clicky-Memory-Game/index.html"
+  },
+  {
+    "revision": "64904ccb735d9acc9ab9",
+    "url": "/Clicky-Memory-Game/static/css/main.edfe0b6c.chunk.css"
+  },
+  {
+    "revision": "8d9cf1cf6c3fc8dc5619",
+    "url": "/Clicky-Memory-Game/static/js/2.d7921de6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Memory-Game/static/js/2.d7921de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64904ccb735d9acc9ab9",
+    "url": "/Clicky-Memory-Game/static/js/main.dd9fd091.chunk.js"
+  },
+  {
+    "revision": "3c73db0e8a750f0b4c00",
+    "url": "/Clicky-Memory-Game/static/js/runtime-main.ea6b19a8.js"
+  }
+]);
